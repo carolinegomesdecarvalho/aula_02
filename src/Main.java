d44c0d6c7ca60541a6f7347df438977bac297bd8
Caroline Gomes de Carvalho <carolinegomesdecarvalho@gmail.com>
2023-11-22 18:35:20 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+//-----Arrays Multidimensionais
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] cars = {{"bmw", "tesla",},{"jeep", "fiat"}};
+        System.out.println(Arrays.deepToString(cars));
+
+    }
+}
